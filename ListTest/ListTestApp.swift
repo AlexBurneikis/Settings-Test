09790523a660fa11e0351325016d27915c5d2086
Burneikis <87457198+AlexBurneikis@users.noreply.github.com>
2022-06-06 16:20:32 +1000

    Initial Commit

---
+++ ListTest/ListTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListTestApp.swift
+//  ListTest
+//
+//  Created by Alexander Burneikis on 6/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct ListTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
